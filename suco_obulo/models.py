216a9b32de196f145d29c5d90d62faf7036556e6
onorio <onorio@catalpa.io>
2024-07-25 22:44:31 +0900

    added category and post models

--- suco_obulo/models.py
+++ suco_obulo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    overview = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField()
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
