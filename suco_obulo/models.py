9a20fa0d279a83fc4562e971d9bc5ec9ec579fe2
onorio <onorio@catalpa.io>
2024-07-25 22:49:23 +0900

    added media folder to gitignore

--- suco_obulo/models.py
+++ suco_obulo/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     overview = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="media/obulo_images")
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
