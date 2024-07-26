f9249838224a4ae7d1581a252c7662db87560d0b
onorio <onorio@catalpa.io>
2024-07-26 14:46:47 +0900

    list_display post in admin

--- suco_obulo/admin.py
+++ suco_obulo/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Post, Category
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author')
+    fieldsets = [
+        (None, {'fields': [('title', 'overview', 'image', 'category')]}),
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'author', None) is None:
+            obj.author = request.user
+        obj.save()
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
--- suco_obulo/urls.py
+++ suco_obulo/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('nature-and-tourism', views.nature_and_tourism_view, name='nature_and_tourism'),
     path('aldeias', views.aldeias_view, name='aldeias'),
+    path('nature-and-tourism', views.nature_and_tourism_view, name='nature_and_tourism'),
     path('people-and-culture', views.people_and_culture_view, name='people_and_culture'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
