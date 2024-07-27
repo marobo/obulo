be5042c0e6afa29a54f66664e179369a73ecbaab
onorio <onorio@catalpa.io>
2024-07-27 13:55:03 +0900

    more static settup

--- obulo/settings.py
+++ obulo/settings.py
@@ -128,7 +128,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
